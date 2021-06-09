49477857ba6d641b6bad55334e929282d035c2c8
Luiz fabiano silva samuel <85593514+LuizGafanhotos@users.noreply.github.com>
2021-06-09 10:25:55 -0300

    Digitei uma função

--- teste c#/arquivo.cs
+++ teste c#/arquivo.cs
@@ -1,11 +1,19 @@
 using System;
 
 class Base{
+    void arquivo(int n1, int n2){
+        return n1 + n2;
+    }
+
+
     public static void Main(){
         byte c = 0;
+        int soma = arquivo(4,4);
         do{
             Console.WriteLine(c + ") Ola mundo");
             c++;
         }while(c <= 5);
+
+        Console.WriteLine(soma);
     }
 }
