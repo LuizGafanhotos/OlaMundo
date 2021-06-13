a27fea4887eca898a22d7ece21d31e53630cd1b6
Luiz fabiano silva samuel <85593514+LuizGafanhotos@users.noreply.github.com>
2021-06-12 21:48:49 -0300

    Binário

--- Curso de linguagem c e c++/MegaFuckingProject/main.c
+++ Curso de linguagem c e c++/MegaFuckingProject/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int soma(int n1, int n2){
+    return n1 + n2;
+}
+
+
 int main()
 {
-    printf("Hello world!\n");
+
+    int calc = soma(41,31);
+    printf("o resultado %d", calc);
+
     return 0;
 }
