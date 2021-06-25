2f5ec7d2e66393c1a35be83320e107fe259b4bbc
unknown <luizfabiano.13lfww@gmail.com>
2021-06-24 21:22:37 -0300

    Mudando um projeto

--- Curso de linguagem c e c++/MegaFuckingProject/main.c
+++ Curso de linguagem c e c++/MegaFuckingProject/main.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma(int n1, int n2){
-    return n1 + n2;
-}
-
-
 int main()
 {
+    int iJ,iI;
+    iJ = 17;
+    iI = 60;
 
-    int calc = soma(41,31);
-    printf("o resultado %d", calc);
+    int idade;
+    printf("Digite a idade de uma \"pessoa\": \n");
+    scanf("%i",&idade);
+
+    if (idade <= iJ){
+        printf("A idade informada e de um jovem ");
+    }else{
+        if (idade >= iI){
+            printf("A informada e de uma pessoa idosa");
+        }else{
+            if ((idade > iJ)&&(idade < iI)){
+                printf("A idade informada e de uma pessoa de meia-idade");
+            }
+        }
+    }
+
 
     return 0;
 }
