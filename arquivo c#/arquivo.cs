f252d25a6b3bab92776f06827dc26ad3f8f16273
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-06-30 10:51:26 -0300

    A base completa
    Isto Para o Curso c#

--- arquivo c#/arquivo.cs
+++ arquivo c#/arquivo.cs
@@ -3,15 +3,20 @@ using System;
 class Base{
 
     public static void Main(){
-        byte c = 0;
+        byte c2 = 0;
         int valor = 11;
         int[] n = new int[5];
-        do{
-            n[c] = valor;
-            valor++;
-            c++;
-        }while(c <= 4);
-        Console.WriteLine(n);
+        try{
+            do{
+                n[c2] = valor;
+                ++valor;
+                ++c2;
+                Console.WriteLine(n[c2]);
+            }while(c2 <= 4);
+        }catch(Exception e){
+            Console.WriteLine("Houve um erro grave.....");
+        }
+        
 
         byte c = 0;
         do{
@@ -20,5 +25,22 @@ class Base{
         }while(c <= 5);
 
         Console.WriteLine("FIM");
+        Console.WriteLine(calc(5,3));
+
+        leia_num();
+    }
+    
+    static int calc(int n1, int n2){
+        int matho = n1 + n2;
+        return matho;
+    }
+
+    static void leia_num(){
+        int v1,v2,res;
+        Console.WriteLine("Digite 2 valores: ");
+        v1= Convert.ToInt32(Console.ReadLine());
+        v2 = Convert.ToInt32(Console.ReadLine());
+        res = v1 + v2; 
+        Console.WriteLine("A soma de {0} e {1} é igual a {2}", v1, v2, res);
     }
 }
