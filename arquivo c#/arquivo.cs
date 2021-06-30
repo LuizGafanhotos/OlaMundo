5f6a571889220eac5610aecb6b924730635d3175
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-06-30 09:10:22 -0300

    Adiciono Comando
    Arquivo C#

--- arquivo c#/arquivo.cs
+++ arquivo c#/arquivo.cs
@@ -4,6 +4,16 @@ class Base{
 
     public static void Main(){
         byte c = 0;
+        int valor = 11;
+        int[] n = new int[5];
+        do{
+            n[c] = valor;
+            valor++;
+            c++;
+        }while(c <= 4);
+        Console.WriteLine(n);
+
+        byte c = 0;
         do{
             Console.WriteLine(c + ") Ola mundo");
             c++;
