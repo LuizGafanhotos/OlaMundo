e9811ad0afb2f612f7cfafdd0bbd1888a7ec73b8
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-09-15 15:44:22 -0300

    Formulário na pratica
    aqui, um arquivo PHP para praticar como se faz um formulário pica em php

--- Curso de php/project/php_code/base.php
+++ Curso de php/project/php_code/base.php
@@ -5,8 +5,24 @@ $categorias[] = "INFANTIL";
 $categorias[] = "ADOLECENTES";
 $categorias[] = "ADULTO";
 
-$nome = "Eduardo";
-$idade = 18;
+$nome = $_POST["nome"] ?? "Eduardo";
+$idade = $_POST["idade"] ?? 7;
+
+if (empty($nome)){
+    echo "O nome não poderá ser vazio<br>";
+    return;
+}
+if (strlen($nome) < 3){
+    echo "O nome deve conter mais de 3 caracteres";
+    return;
+}
+if (strlen($nome) > 40){
+    echo "O nome é absurdamente extenso"; //Extenso pra krl mas enfim
+}
+if (!is_numeric($idade)){
+    echo "Informe um numero para a idade";
+    return;
+}
 
 if($idade >= 6 && $idade <= 12){
     for($l =0; $l <= count($categorias); $l++){
@@ -16,8 +32,8 @@ if($idade >= 6 && $idade <= 12){
     }
 }else if ($idade >= 13 && $idade <= 18){
     for($l = 0; $l <= count($categorias); $l++){
-        if ($categorias[$l] == "ADOLECENTES"){
-            echo "O nadador ".$nome. "compete na categoria adolecente";
+        if ($categorias[$l] == "ADOLECENTES") {
+            echo "O nadador " .$nome. "compete na categoria adolecente";
         }
     }
 }else{
---
+++ Curso de php/project/php_code/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário</title>
+</head>
+<body>
+    <p>FORMULÁRIO PARA A COMPETIÇÃO</p>
+    <form action="base.php" method="post">
+        <p>Seu nome: <input type="text" name="nome"></p>
+        <p>Sua idade: <input type="text" name="idade"></p>
+        <input type="submit" value="Mandar">
+    </form>
+</body>
+</html>
