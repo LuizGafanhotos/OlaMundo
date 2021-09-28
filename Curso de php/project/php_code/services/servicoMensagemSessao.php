489a9f19267d2fb3c2a996d523ac85c37fe7d665
LuizGafanhotos <luizfabiano.13lfww@gmail.com>
2021-09-28 15:02:06 -0300

    Continuando o cadastro em php

--- Curso de php/project/php_code/services/servicoMensagemSessao.php
+++ Curso de php/project/php_code/services/servicoMensagemSessao.php
@@ -1,6 +1,16 @@
 <?php
 session_start();
 
+function setarMensagemSucesso(string $mensagem) : void{
+    $_SESSION['mensagem-de-sucesso'] = $mensagem;
+}
+function obterMensagemSucesso() : ?string {
+    if(isset($_SESSION['mensagem-de-sucesso'])){
+        return $_SESSION['mensagem-de-sucesso'];
+    }
+    return null;
+}
+
 function setarMensagemErro(string $mensagem) : void{
     $_SESSION['mensagem-de-erro'] = $mensagem;
 }
--- Curso de php/project/php_code/services/servicoValidacao.php
+++ Curso de php/project/php_code/services/servicoValidacao.php
@@ -2,22 +2,22 @@
 declare("strict_type = 1");
 function validaNome(string $nome) : bool{
     if (empty($nome)){
-        $_SESSION["mensagem de erro"] = " O nome não pode estar vazio: por favor preencha novamente";
+        setarMensagemErro(" O nome não pode estar vazio: por favor preencha novamente");
         return false; 
     }
     else if (strlen($nome) < 3){
-        $_SESSION["mensagem-de-erro"] = " O nome deve conter mais de 3 caracteres: por favor preencha novamente";
+        setarMensagemErro(" O nome deve conter mais de 3 caracteres: por favor preencha novamente");
         return false;
     }
     else if (strlen($nome) > 40){
-        $_SESSION["mensagem-de-erro"] = " O nome é absurdamente extenso: por favor preencha novamente";
-        return false; 
+        setarMensagemErro(" O nome é absurdamente extenso: por favor preencha novamente");
+        re  turn false; 
     }
     return true;
 }
 function validaIdade(string $idade) : bool{
     if (!is_numeric($idade)){
-        $_SESSION["mensagem-de-erro"] = " Informe um numero para a idade";
+        setarMensagemErro(" Informe um numero para a idade");
         return false; 
     }
     return true;    
